92bc9d24d1057cbe8b865ba54487af579f36ff1a
mhosain <mohammadtaiby13785@gmail.com>
2024-03-24 12:37:42 +0330

    last update on windows

--- text_bin.c
+++ text_bin.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
 #include <math.h>
 
+char arg_check(char arg[])
+{
+    if((arg[0] == '-') && (arg[1] == 'b'))
+    {
+        return 'b';
+    }
+    else if ((arg[0] == '-') && (arg[1] == 't'))
+    {
+        return 't';
+    }
+    else if ((arg[0] == '-') && (arg[1] == 'h'))
+    {
+        return 'h';
+    }
+    else {
+        return 'o';
+    }
+}
+
 int main(int argc, char *argv[])
 {
 
     unsigned short int value = 0;
 
-    // printf("%s", argv[1]);
+    // printf("%d\n", argc);
+    // for(unsigned short int i = 1; i < argc; i++){
+    //     printf("%s\n", argv[i]);
+    // }
 
-    if (argc > 1)
+    if ((argc == 1) && (arg_check(argv[1]) == 'b'))
     {
-        // printf("%c", argv[1][4]);
-        for (unsigned short int i = 0; i < (argc - 1); i++)
+        printf("TBED (Text to Binary decode and vise versa): \n");
+        printf("-b: binary input\n");
+        printf("-t: text input\n");
+        printf("-h: prints this dialog and exits\n");
+        printf("\n");
+    }
+
+    if ((argc > 1) && (argv[1] != "-b"))
+    {
+        printf("%s\n", argv[1]);
+        for (unsigned short int i = 2; i < argc; i++)
         {
             for (unsigned short int j = 0; j < 8; j++)
             {
-                if (argv[i + 1][j] == '1')
+                if (argv[i][j] == '1')
                 {
                     value += pow(2, (7 - (j)));
                 }
@@ -25,6 +56,7 @@ int main(int argc, char *argv[])
                 }
             }
             printf("%c", (char)value);
+            value = 0;
         }
     }
 
